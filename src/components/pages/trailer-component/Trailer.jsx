c12e776afd3a606435b2de204ae0fa0d3ead77a7
Rajat Dey <Rajatdey12@github.com>
2025-07-13 23:36:02 +0530

    changed titles

--- src/components/pages/trailer-component/Trailer.jsx
+++ src/components/pages/trailer-component/Trailer.jsx
@@ -7,10 +7,10 @@ function Trailer() {
         <>
             <div className='trailer-container'>
                 <video className='tailer-video' src='/videos/video-3.mp4' autoPlay loop muted />
-                <h1>Witness the true happiness Just Starting at Rs 199/- only.</h1>
+                <h1>Witness the true happiness Just Starting at Rs 1999/- only.</h1>
                 <p>
-                    Spread across 1.2 acres of land just waiting for you.
-                    So what are you waiting for. Go to <Link to='/services'>services</Link> and enroll Today.
+                    Spread across 1.2 acres of land with lots of fun activities just waiting for you.
+                    So what are you waiting for, go and <Link to='/services'>ENROLL</Link> Today.
                 </p>
             </div>
         </>
